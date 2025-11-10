1b7e235b427648bdf1a6f6d8ce7b80b16f009b33
maikelmaikel1234 <maikelschepers18@outlook.com>
2025-11-10 14:08:21 +0100

    devpro dag3

---
+++ src/main/java/org/example/HomePage.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HomePage extends Page{
+
+    public HomePage(HttpServletRequest request, HttpServletResponse response) {
+        super(request, response);
+    }
+
+    @Override
+    public void render() throws IOException {
+        HttpSession session = request.getSession(false);
+//        if (session != null && session.getAttribute("username") == null) {
+//            response.sendRedirect("/login");
+//            return;
+//        }
+
+        PrintWriter out = getWriter();
+        String language = getLanguage();
+        String theme = getTheme();
+        String username = getUsername();
+
+        // Gebruik language en theme om je HTML te genereren
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<style>");
+        if ("dark".equals(theme)) {
+            out.println("body { background: #222; color: #eee; }");
+        } else {
+            out.println("body { background: #fff; color: #222; }");
+        }
+        out.println("</style>");
+        out.println("</head>");
+        out.println("<body>");
+
+        out.println("<div id='stap1'>");
+        if ("en".equals(language)) {
+            out.println("<h1>Welcome " + username + "!</h1>");
+            out.println("<p>Step 1: Personal information</p>");
+        } else {
+            out.println("<h1>Welkom " + username + "!</h1>");
+            out.println("<p>Stap 1: Persoonlijke informatie</p>");
+        }
+        out.println("<form method='post' action='/survey/step1'>");
+        out.println("<label for= 'naam'>Naam: </label>");
+        out.println("<input type = 'text' name = 'naam'/><br>");
+        out.println("<label for= 'e-mail'>e-mail: </label>");
+        out.println("<input type = 'text' name = 'e-mail'/><br>");
+        out.println("<button id='button' type='submit' value='Submit'>Submit</button>");
+        out.println("</form>");
+        out.println("</div>");
+
+        out.println("<div id='stap2'>");
+        out.println("<p> hallo </p>");
+        out.println("</div>");
+
+        out.println("<script src='app.js'></script>");
+
+        out.println("</body></html>");
+    }
+}
+
