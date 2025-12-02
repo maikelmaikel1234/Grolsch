ad6ada7255bc6225264e9d5b1f44b6b566ce96de
Peter van Engeland <peter.van.engeland@topicus.nl>
2025-12-02 20:46:43 +0100

    stappen_werkend: ga na preferences naar ajaxHome en niet naar stap1

--- src/main/java/org/example/PreferencesServlet.java
+++ src/main/java/org/example/PreferencesServlet.java
@@ -54,7 +54,7 @@ public class PreferencesServlet extends HttpServlet {
         response.addCookie(createCookie("language", language));
         response.addCookie(createCookie("theme", theme));
 
-        response.sendRedirect("/survey/step1");
+        response.sendRedirect("/ajaxHome");
     }
 
     private Cookie getCookie(HttpServletRequest request, String cookieName, String defaultValue)
